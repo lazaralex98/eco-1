1102204639473bfdd0d0abb926bd3a543c150a46
lazaralex98 <alex2012ro@gmail.com>
2022-01-26 03:25:37 +0200

    tremendously simplified, forgot about sushi, etc

--- contracts/WTCO2.sol
+++ contracts/WTCO2.sol
@@ -3,45 +3,51 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// TODO: how do you even import these with hardhat?!?
+// TODO: how do you even import from https with hardhat?!?
 import "https://github.com/CO2ken/contracts/blob/602ac834cd18b6bd9d863ed8c5b6f9be9efa9fc0/contracts/ToucanCarbonOffsets.sol#L221";
 import "https://github.com/CO2ken/contracts/blob/602ac834cd18b6bd9d863ed8c5b6f9be9efa9fc0/contracts/pools/BaseCarbonTonne.sol#L362";
 import "https://github.com/sushiswap/sushiswap/blob/canary/contracts/SushiMaker.sol";
 
-contract WTCO2 is ToucanCarbonOffsets {
+contract WTCO2 {
   uint256 private footprint;
-  address private sushiMakerAddress = "0xf1c9881Be22EBF108B8927c4d197d126346b5036";
-  address private baseCarbonTonneAddress = "";
+  uint256 private tcoAddress = "TODO insert TCO2 address";
+  ToucanCarbonOffsets public tco;
+  event Retirement(address indexed sender, uint256 amount);
 
-  // TODO: this is hardcoded for now
-  function calculateFootprint() public pure returns (uint256) {
-    return 10;
+  constructor() public {
+    tco = new ToucanCarbonOffsets();
   }
 
-  function deposit(uint256 _amount, address _receivedToken) public payable {
-    if (
-      // TODO: how do I even check this?!? ETH is not a token 🤯
-      _receivedToken == "TODO_ETH_address" ||
-      _receivedToken == "TODO_MATIC_address" ||
-      _receivedToken == "TODO_USDC_address"
-    ) {
-      /**
-       * TODO: my though for doing this is to instantiate SushiMaker and use it to convert ETH to BCT.
-       * the problem with that is I need access to the _swap() method
-       */
-      SushiMaker sushi = SushiMaker(sushiMakerAddress);
-      sushi.convert(_receivedToken, baseCarbonTonneAddress);
-    
-    } else if (_receivedToken == baseCarbonTonneAddress) {
-      footprint = calculateFootprint();
+  // TODO: this is hardcoded for now
+  function calculateFootprint(uint256 _tokenAmount)
+    public
+    pure
+    returns (uint256)
+  {
+    return _tokenAmount;
+  }
 
-      /**
-       * TODO: same as above, I figured I should instantiate BCT and use that to redeem TCO2.
-       * BCT for TCO2 using BaseCarbonTonne.redeemMany(). Marcel
-       */
-      BaseCarbonTonne bct = BaseCarbonTonne(baseCarbonTonneAddress);
+  /**
+   * Takes in an amount of TCO2, calculates the footprint and retires
+   * an amount of TCO2 equivalent to the footprint
+   */
+  function retireTCO2(uint256 _amount) external payable {
+    // requirements to make sure we did receive TCO2 tokens
+    require(_amount > 0, "You need at least a few tokens");
+    uint256 allowance = tco.allowance(msg.sender, address(this));
+    require(allowance >= _amount, "Check the token allowance");
 
-      // TODO retire TCO2
-      retire(footprint);
-    }
+    // calculate footprint
+    footprint = calculateFootprint(_amount);
+
+    // transfer TCO2 to this contract
+    tco.transferFrom(msg.sender, address(this), _amount);
+    msg.sender.transfer(_amount);
+
+    // retire an amount of TCO2 equal to the calculated footprint
+    tco.retire(footprint);
+
+    // emit a Retirement event
+    emit Retirement(msg.sender, footprint);
+  }
 }
