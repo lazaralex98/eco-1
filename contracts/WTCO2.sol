a2e5192a5291fb76c1e4d48d4cf7c32a344dc7af
lazaralex98 <alex2012ro@gmail.com>
2022-01-26 04:46:08 +0200

    the contract compiles! let's test it! :)

--- contracts/WTCO2.sol
+++ contracts/WTCO2.sol
@@ -2,19 +2,16 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// TODO: how do you even import from https with hardhat?!?
-import "https://github.com/CO2ken/contracts/blob/602ac834cd18b6bd9d863ed8c5b6f9be9efa9fc0/contracts/ToucanCarbonOffsets.sol#L221";
-import "https://github.com/CO2ken/contracts/blob/602ac834cd18b6bd9d863ed8c5b6f9be9efa9fc0/contracts/pools/BaseCarbonTonne.sol#L362";
-import "https://github.com/sushiswap/sushiswap/blob/canary/contracts/SushiMaker.sol";
+// I've no idea how to import from Github. So I just copied these contracts over
+import "./CO2KEN_contracts/ToucanCarbonOffsets.sol";
+import "./CO2KEN_contracts/pools/BaseCarbonTonne.sol";
 
 contract WTCO2 {
   uint256 private footprint;
-  uint256 private tcoAddress = "TODO insert TCO2 address";
   ToucanCarbonOffsets public tco;
   event Retirement(address indexed sender, uint256 amount);
 
-  constructor() public {
+  constructor() {
     tco = new ToucanCarbonOffsets();
   }
 
@@ -31,7 +28,7 @@ contract WTCO2 {
    * Takes in an amount of TCO2, calculates the footprint and retires
    * an amount of TCO2 equivalent to the footprint
    */
-  function retireTCO2(uint256 _amount) external payable {
+  function retireTCO2(uint256 _amount) public payable {
     // requirements to make sure we did receive TCO2 tokens
     require(_amount > 0, "You need at least a few tokens");
     uint256 allowance = tco.allowance(msg.sender, address(this));
@@ -42,7 +39,7 @@ contract WTCO2 {
 
     // transfer TCO2 to this contract
     tco.transferFrom(msg.sender, address(this), _amount);
-    msg.sender.transfer(_amount);
+    tco.transfer(address(this), _amount);
 
     // retire an amount of TCO2 equal to the calculated footprint
     tco.retire(footprint);
