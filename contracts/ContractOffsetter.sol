074b4f18582b7a8d3f72eb95c74f273c5922bd56
lazaralex98 <alex2012ro@gmail.com>
2022-02-10 17:57:26 +0200

    Contract Offsetter + tests boilerplate

---
+++ contracts/ContractOffsetter.sol
@@ -0,0 +1,128 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "./CO2KEN_contracts/ToucanCarbonOffsets.sol";
+import "./CO2KEN_contracts/pools/BaseCarbonTonne.sol";
+import "./CO2KEN_contracts/IToucanContractRegistry.sol";
+
+contract ContractOffsetter is OwnableUpgradeable {
+    using SafeERC20 for IERC20;
+    address public bctAddress = 0xf2438A14f668b1bbA53408346288f3d7C71c10a1;
+    address public contractRegistry = 0x6739D490670B2710dc7E79bB12E455DE33EE1cb6;
+    // user => (token => amount)
+    mapping(address => mapping(address => uint256)) public balances;
+
+    event Deposited(
+        address depositor,
+        address erc20Address,
+        uint256 amountDeposited
+    );
+    event Redeemed(
+        address redeemer,
+        address receivedTCO2,
+        uint256 amountRedeemed
+    );
+    event Offset(
+        address offsetter,
+        address retiredTCO2,
+        uint256 amountOffset
+    );
+
+    // @description you can use this to change the TCO2 contracts registry if needed
+    // @param _address the contract registry to use
+    function setToucanContractRegistry(address _address)
+    public
+    virtual
+    onlyOwner
+    {
+        contractRegistry = _address;
+    }
+
+    // @description checks if token to be deposited is eligible for this pool
+    // @param _erc20Address address to be checked
+    function checkTokenEligibility(address _erc20Address)
+    private
+    view
+    returns (bool)
+    {
+        // check if token is a TCO2
+        bool isToucanContract = IToucanContractRegistry(contractRegistry)
+        .checkERC20(_erc20Address);
+        if (isToucanContract) return true;
+
+        // check if token is BCT
+        if (_erc20Address == bctAddress) return true;
+
+        // nothing matches, return false
+        return false;
+    }
+
+    // @description deposit tokens from use to this contract
+    // @param _erc20Address token to be deposited
+    // @param _amount amount to be deposited
+    function deposit(address _erc20Address, uint256 _amount) public {
+        bool eligibility = checkTokenEligibility(_erc20Address);
+        require(eligibility, "Can't deposit this token");
+
+        // use token's contract to do a safe transfer from the user to this contract
+        // remember that the user has to approve this in the frontend
+        IERC20(_erc20Address).safeTransferFrom(msg.sender, address(this), _amount);
+
+        // add amount of said token to balance of this user in this contract
+        balances[msg.sender][_erc20Address] += _amount;
+
+        emit Deposited(msg.sender, _erc20Address, _amount);
+    }
+
+    // @description redeems some BCT from contract balance for a chosen TCO2 token
+    // @param _desiredTCO2 the address of the TCO2 you want to receive
+    // @param _amount the amount of BCT you want to redeem for TCO2
+    function redeemBCT(address _desiredTCO2, uint256 _amount) public {
+        require(
+            _amount <= balances[msg.sender][bctAddress],
+            "You don't have enough BCT in this contract."
+        );
+
+        bool eligibility = checkTokenEligibility(_desiredTCO2);
+        require(eligibility, "Can't redeem BCT for this token.");
+
+        // prepare/format params for BCT.retireMany() method
+        address[] memory tco2Addresses = new address[](1);
+        uint256[] memory amounts = new uint256[](1);
+        tco2Addresses[0] = _desiredTCO2;
+        amounts[0] = _amount;
+
+        // send BCT, receive TCO2
+        BaseCarbonTonne(bctAddress).redeemMany(tco2Addresses, amounts);
+
+        // modify balance sheets of this contract
+        balances[msg.sender][bctAddress] -= _amount;
+        balances[msg.sender][_desiredTCO2] += _amount;
+
+        emit Redeemed(msg.sender, _desiredTCO2, _amount);
+    }
+
+    // @description retire TCO2 so that you offset a certain amount of the carbon used by this contract
+    // @param _tco2Address address of the TCO2 you want to retire
+    // @param _amount how much CO2 you want to offset
+    function offset(address _tco2Address, uint256 _amount) public {
+        bool eligibility = checkTokenEligibility(_tco2Address);
+        require(eligibility, "Can't retire this token.");
+
+        require(
+            _amount <= balances[msg.sender][_tco2Address],
+            "You don't have enough of this TCO2 (in the contract)."
+        );
+
+        // use the TCO contract to retire TCO2
+        ToucanCarbonOffsets(_tco2Address).retire(_amount);
+
+        // reduce amount of TCO2 in the balance sheet
+        balances[msg.sender][_tco2Address] -= _amount;
+
+        emit Offset(msg.sender, _tco2Address, _amount);
+    }
+}
--- test/index.ts
+++ test/index.ts
@@ -4,9 +4,11 @@ import { ethers, network } from "hardhat";
 // eslint-disable-next-line node/no-missing-import,camelcase
 import {
   BaseCarbonTonne,
+  ContractOffsetter,
   ContractOffsetterPOC,
   // eslint-disable-next-line camelcase
   ContractOffsetterPOC__factory,
+  ContractOffsetter__factory,
   ToucanCarbonOffsets,
 } from "../typechain";
 import * as tcoAbi from "../artifacts/contracts/CO2KEN_contracts/ToucanCarbonOffsets.sol/ToucanCarbonOffsets.json";
@@ -25,8 +27,6 @@ const tco2Address: string = "0xa5831eb637dff307395b5183c86B04c69C518681";
 // and this is the address that I wish to deploy from
 const myAddress: string = "0x721F6f7A29b99CbdE1F18C4AA7D7AEb31eb2923B";
 const bctAddress: string = "0xf2438A14f668b1bbA53408346288f3d7C71c10a1";
-
-// TODO more tests
 
 describe("Contract Offsetter POC", function () {
   let cop: ContractOffsetterPOC;
@@ -204,5 +204,110 @@ describe("Contract Offsetter POC", function () {
         )
       ).to.be.revertedWith("You can't offset more than your footprint.");
     });
+  });
+});
+
+describe("Contract Offsetter", function () {
+  let co: ContractOffsetter;
+  let tco: ToucanCarbonOffsets;
+  let bct: BaseCarbonTonne;
+  let owner: SignerWithAddress;
+  let addr1: SignerWithAddress;
+  let addr2: SignerWithAddress;
+  let addrs: SignerWithAddress[];
+
+  beforeEach(async function () {
+    /**
+     * if we are forking Mumbai (which I chose to do for performance, so I can test & iterate faster)
+     * we impersonate my Mumbai account (I have TCO2, BCT & MATIC on it at the blockNumber I chose)
+     */
+    if (network.name === "hardhat") {
+      await network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: [myAddress],
+      });
+    }
+
+    // we get a signer based on my above address (I have TCO2, BCT & MATIC on it at the blockNumber I chose)
+    owner = await ethers.getSigner(myAddress);
+    // and we get a bunch of other random signers
+    [addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    // we deploy a DEX contract and get a portal to it
+    const coFactory = (await ethers.getContractFactory(
+      "ContractOffsetter",
+      owner
+      // eslint-disable-next-line camelcase
+    )) as ContractOffsetter__factory;
+    co = await coFactory.deploy();
+
+    // we instantiate a portal to my TCO2 contract
+    // @ts-ignore
+    tco = new ethers.Contract(tco2Address, tcoAbi.abi, owner);
+
+    // we instantiate a portal to BCT
+    // @ts-ignore
+    bct = new ethers.Contract(bctAddress, bctAbi.abi, owner);
+  });
+
+  describe("Deposit", function () {
+    /**
+     * We're depositing 1 TCO2 to a newly deployed contract, empty contract
+     */
+    it("Should deposit 1 TCO2", async function () {
+      await deposit(tco, co, tco2Address, "1.0");
+
+      const balance = await co.balances(myAddress, tco2Address);
+      expect(ethers.utils.formatEther(balance)).to.eql("1.0");
+    });
+
+    it("Should deposit 1 BCT", async function () {
+      await deposit(bct, co, bctAddress, "1.0");
+
+      const balance = await co.balances(myAddress, bctAddress);
+      expect(ethers.utils.formatEther(balance)).to.eql("1.0");
+    });
+
+    it("Should not accept LINK", async function () {
+      await expect(
+        co.deposit(
+          "0x514910771af9ca656af840dff83e8264ecf986ca",
+          ethers.utils.parseEther("0.5"),
+          {
+            gasLimit: 1200000,
+          }
+        )
+      ).to.be.revertedWith("Can't deposit this token");
+    });
+  });
+
+  describe("redeemBCT", function () {
+    it("Should redeem 1 BCT for 1 TCO2", async function () {
+      await deposit(bct, co, bctAddress, "10.0");
+
+      const redeemTxn = await co.redeemBCT(
+        tco2Address,
+        ethers.utils.parseEther("1.0")
+      );
+      await redeemTxn.wait();
+
+      const bctBalance = await co.balances(myAddress, bctAddress);
+      expect(ethers.utils.formatEther(bctBalance)).to.eql("9.0");
+
+      const tcoBalance = await co.balances(myAddress, tco2Address);
+      expect(ethers.utils.formatEther(tcoBalance)).to.eql("1.0");
+    });
+
+    it("Should be reverted with 'You don't have enough BCT in this contract.'", async function () {
+      await deposit(bct, co, bctAddress, "0.5");
+
+      await expect(
+        co.redeemBCT(tco2Address, ethers.utils.parseEther("10.0"))
+      ).to.be.revertedWith("You don't have enough BCT in this contract.");
+    });
+  });
+
+  describe("offset", function () {
+    // TODO write tests for offset()
   });
 });
