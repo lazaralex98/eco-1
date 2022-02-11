14f3b608c6e6a138a40c81dccfe5f24e75cb90c9
lazaralex98 <alex2012ro@gmail.com>
2022-02-11 15:04:59 +0200

    deployed contractOffsetter

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,14 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const ContractOffsetter = await ethers.getContractFactory(
+    "ContractOffsetter"
+  );
+  const co = await ContractOffsetter.deploy();
 
-  await greeter.deployed();
+  await co.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("ContractOffsetter deployed to:", co.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
